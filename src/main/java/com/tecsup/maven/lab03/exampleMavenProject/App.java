489057cdb390f1965bd2b5abf7794c508a1c5f8b
Alumno <Alumno@LAB1507-13>
2023-04-11 13:59:44 -0500

    First Commit

---
+++ src/main/java/com/tecsup/maven/lab03/exampleMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.maven.lab03.exampleMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
